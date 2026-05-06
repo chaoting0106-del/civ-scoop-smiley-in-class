b5467bb73801993a590887f1fbcf6c0444d2afed
Chao-Ting Yen <J277184@tafe.wa.edu.au>
2026-05-06 15:16:59 +0800

    create angry

---
+++ angry.py
@@ -0,0 +1,49 @@
+import time
+from blinkable import Blinkable
+from smiley import Smiley
+
+
+class Angry(Smiley, Blinkable):
+    """
+   Provides a Smiley with a angry expression
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        """
+       Renders a mouth by blanking the pixels that form that object.
+        """
+        mouth = [41, 54, 50, 43, 52, 45]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+       Draws the eyes (open or closed) on the standard smiley.
+        :param wide_open (bool): eyes open or closed.
+        """
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            if wide_open:
+                eyes = self.BLANK
+            else:
+                eyes = self.RED
+            self.pixels[pixel] = eyes
+
+    def blink(self, delay=0.25):
+        """
+       Blinks the smiley's eyes once
+        
+        :param delay: Delay between blinks (in seconds)
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
+
+
